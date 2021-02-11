0ecd718193fc34f7c5a63f631ad6934050088c97
FilipJQ77 <filipprzygonski99@gmail.com>
2021-02-11 20:58:42 +0100

    minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,6 @@ function log() {
     console.log("A");
 }
 
-var interval2ID = window.setInterval(sound, 100);
-
 var audio = new Audio('https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/6d/Cave2.ogg/revision/latest');
 
 function sound() {
