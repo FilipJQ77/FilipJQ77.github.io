ac6a089b13865f23f622e74b215c30121378e3aa
FilipJQ77 <filipprzygonski99@gmail.com>
2021-04-14 15:56:59 +0200

    change lang to en, more informative content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-var intervalID = window.setInterval(log, 1000);
-
-function log() {
-    console.log("A");
-}
-
 var audio = new Audio("https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d3/Cave1.ogg");
 
 function sound() {
